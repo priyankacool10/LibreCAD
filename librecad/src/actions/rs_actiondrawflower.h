5f68c1508d87390c94319020d107b8c089f8b236
priyankacool10 <anjalicool.kapoor444@gmail.com>
2014-04-10 20:05:47 +0530

    New Flower Pattern

--- librecad/src/actions/rs_actiondrawflower.h
+++ librecad/src/actions/rs_actiondrawflower.h
@@ -3,7 +3,7 @@
 ** This file is part of the LibreCAD project, a 2D CAD program
 **
 ** Copyright (C) 2010 R. van Twisk (librecad@rvt.dds.nl)
-** Copyright (C) 2001-2003 RibbonSoft. All rights reserved.
+** Copyright (C) 2014 Priyanka Kapoor (anjalicool.kapoor444@gmail.com)
 **
 **
 ** This file may be distributed and/or modified under the terms of the
@@ -34,7 +34,7 @@
 * This action class can handle user events to draw
 * flower with a given center, radius and number of petals.
 *
-* @author Priyanka Kapoor(priyankakapoor590@gmail.com)
+* @author Priyanka Kapoor(anjalicool.kapoor444@gmail.com)
 */
 class RS_ActionDrawFlower : public RS_PreviewActionInterface {
    
